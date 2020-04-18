c7fb4dd497a968be13cd14792c078a6939b16f7f
Andrius Raulinaitis <araulinaitis@gmail.com>
2020-04-17 17:38:01 -0700

    Coding Challenge #8 Solar System (3D)

--- Coding Challenge #8 Solar System (3D)/Solar_System_3D_Main.m
+++ Coding Challenge #8 Solar System (3D)/Solar_System_3D_Main.m
@@ -1,40 +1,37 @@
-clear all; clc; close all;
+clear all; clc; close all;clear all; clc; close all;
 
-figure('Color', 'k');
+figure('Color', 'k', 'WindowState', 'maximized');
 axis equal
 axis off
+axis([-1000, 1000, -1000, 1000, -1000, 1000]);
+% colormap([1, 1, 1]);
+light
 axis manual
-axis([-1000, 1000, -1000, 1000]);
+hold on
 
-frameRate = 15;
+frameRate = 60;
 
-writerObj = VideoWriter('SolarSystem2D.avi');
+writerObj = VideoWriter('SolarSystem3D.avi');
 writerObj.FrameRate = frameRate;
 
 open(writerObj);
 
 % Make sun
-sun = Body(0, 0, 100, []);
+sun = Body3D(0, 0, 0, 100, []);
 
-for i = 1:randi([1, 2])
-    sun.addChild;        
-end
+sun.addChildren(randi([1, 2]));
 
 for i = 1:length(sun.children)
-    for j = 1:randi([1, 2])
-        sun.children(i).addChild; 
-    end
+    sun.children(i).addChildren(randi([1, 2]));
 end
 
 for i = 1:length(sun.children)
     for j = 1:length(sun.children(i).children)
-        for k = 1:randi([1, 3])
-            sun.children(i).children(j).addChild;
-        end
+        sun.children(i).children(j).addChildren(randi([1, 3]));
     end
 end
     
-for idx = 1:500
+for idx = 1:1000
 % while 1
     startLoop = now;
     
@@ -47,6 +44,8 @@ for idx = 1:500
     
     while (now - startLoop) * 10^5 < (1 / frameRate)
     end
+   
+%     1/((now - startLoop) * 10^5)
     
 end
 
